ac2672b075ffd78db3193abf0c6cc3e09a3045df
danderu <mail@dandel.es>
2017-03-26 21:58:20 +0200

    Simplified normalizr

--- src/getInitialState.js
+++ src/getInitialState.js
@@ -2,17 +2,16 @@ import  { schema, normalize } from 'normalizr'
 import originalData from './schema.json'
 
 export default function getInitialState () {
-  const groupedBlock = new schema.Entity('fields')
+  const block = new schema.Entity('fields')
+  const blockList = new schema.Array(block)
+  
+  block.define({
+    fields: blockList
+  })
 
-  // Define a block schema
-  const block = new schema.Entity('fields', {
-    fields: [ groupedBlock ]
-  });
-
-  // Define the form
   const form = new schema.Entity('form', {
-    fields: [ block ]
+    fields: blockList
   });
 
-  return normalize(originalData, form).entities;
+  return normalize(originalData, form);
 }
